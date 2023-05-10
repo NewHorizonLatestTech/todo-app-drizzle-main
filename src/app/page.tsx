062d795c1fb8620b9ba1d75c434615e2db04ecd1
Adeema Amir <adeemaamir1722@gmail.com>
2023-05-10 09:08:58 +0500

    index

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+// {
+
+import TodoUI from "./Todo";
+
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <TodoUI />
+    </main>
+  );
+}
+
+// }
